2bfb3102419019f37b27b3571c2350c139004981
educ8s <info@educ8s.tv>
2022-10-04 08:14:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,98 +1,125 @@
 import pygame, sys, random
 
-def game_over(winner):
-    global ball_speed_y, ball_speed_x, cpu_points, player_points
+def reset_ball():
+    global ball_speed_x, ball_speed_y
+    ball.x = screen_width/2 - 10
+    ball.y = random.randint(10,100)
+    ball_speed_x *= random.choice([-1,1])
+    ball_speed_y *= random.choice([-1,1])
+
+def point_won(winner):
+    global cpu_points, player_points
+
     if winner == "cpu":
         cpu_points += 1
     if winner == "player":
         player_points += 1
-    ball.x = screen_width/2 - 10
-    ball.y = random.randint(10,100)
-    ball_speed_x *= random.choice([1,-1])
-    ball_speed_y *= random.choice([1,-1])
+
+    reset_ball()
 
 def animate_ball():
     global ball_speed_x, ball_speed_y
     ball.x += ball_speed_x
     ball.y += ball_speed_y
+
     if ball.bottom >= screen_height or ball.top <= 0:
         ball_speed_y *= -1
-    if ball.right >= screen_width: 
-        game_over("cpu")
+
+    if ball.right >= screen_width:
+        point_won("cpu")
+
     if ball.left <= 0:
-        game_over("player")
-    if ball.colliderect(player):
-        ball_speed_x *= -1
-    if ball.colliderect(opponent):
+        point_won("player")
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    if ball.colliderect(player) or ball.colliderect(cpu):
         ball_speed_x *= -1
 
 def animate_player():
-    player.y += player_speed 
+    player.y += player_speed
+
     if player.top <= 0:
         player.top = 0
+
     if player.bottom >= screen_height:
         player.bottom = screen_height
 
 def animate_cpu():
     global cpu_speed
-    opponent.y += cpu_speed
-    if opponent.bottom >= ball.bottom:
-        cpu_speed = - 7
-    if opponent.top <= ball.top:
-        cpu_speed = 7
-    if opponent.top <= 0:
-        opponent.top = 0
-    if opponent.bottom >= screen_height:
-        opponent.bottom = screen_height
+    cpu.y += cpu_speed
+
+    if ball.centery <= cpu.centery:
+        cpu_speed = -6
+    if ball.centery >= cpu.centery:
+        cpu_speed = 6
+
+    if cpu.top <= 0:
+        cpu.top = 0
+    if cpu.bottom >= screen_height:
+        cpu.bottom = screen_height
 
 pygame.init()
-clock = pygame.time.Clock()
-score_font = pygame.font.Font(None , 100)
-screen_width, screen_height = 1280, 960
-cpu_points, player_points = 0, 0
 
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen_width = 1280
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("My Pong Game!")
 
-# Game Assets
-ball = pygame.Rect(screen_width/2 - 10, screen_height/2 - 10, 20, 20)
-ball_speed_x = 7
-ball_speed_y = 7
-player_speed = 0
-cpu_speed = 0
+clock = pygame.time.Clock()
 
-player = pygame.Rect(screen_width - 30, screen_height/2 - 50, 20, 120)
-opponent = pygame.Rect(10, screen_height/2 - 50, 20, 120)
+ball = pygame.Rect(0,0,30,30)
+ball.center = (screen_width/2, screen_height/2)
+
+cpu = pygame.Rect(0,0,20,100)
+cpu.centery = screen_height/2
+
+player = pygame.Rect(0,0,20,100)
+player.midright = (screen_width, screen_height/2)
+
+ball_speed_x = 6
+ball_speed_y = 6
+player_speed = 0
+cpu_speed = 6
+
+cpu_points, player_points = 0, 0
+
+score_font = pygame.font.Font(None, 100)
 
 while True:
-
+    #Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                player_speed -=7
+                player_speed = -6
             if event.key == pygame.K_DOWN:
-                player_speed += 7
-        
+                player_speed = 6
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
-                player_speed += 7 
+                player_speed = 0
             if event.key == pygame.K_DOWN:
-                player_speed -= 7
+                player_speed = 0
 
-    screen.fill("black")
-    cpu_score_surface = score_font.render(str(cpu_points), True, "white")
-    player_score_surface = score_font.render(str(player_points), True, "white")
-    screen.blit(cpu_score_surface,(screen_width/4,20))  
-    screen.blit(player_score_surface,(3*screen_width/4,20)) 
+    #Change the positions of the game objects
     animate_ball()
     animate_player()
     animate_cpu()
-    pygame.draw.ellipse(screen,"white",ball)
-    pygame.draw.rect(screen, "white", player)
-    pygame.draw.rect(screen, "white", opponent)
-    pygame.draw.aaline(screen, "white", (screen_width/2, 0), (screen_width/2,screen_height))
-    pygame.display.flip()
+        
+    #Draw the game objects
+    screen.fill('black')
+
+    cpu_score_surface = score_font.render(str(cpu_points), True, "white")
+    player_score_surface = score_font.render(str(player_points), True, "white")
+    screen.blit(cpu_score_surface,(screen_width/4,20))
+    screen.blit(player_score_surface,(3*screen_width/4,20))
+
+    pygame.draw.aaline(screen,'white',(screen_width/2, 0), (screen_width/2, screen_height))
+    pygame.draw.ellipse(screen,'white',ball)
+    pygame.draw.rect(screen,'white',cpu)
+    pygame.draw.rect(screen,'white',player)
+
+    #Update the display
+    pygame.display.update()
     clock.tick(60)
